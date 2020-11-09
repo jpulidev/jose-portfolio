784512ff6e17898b4fd3dc5c818ff984efc99952
Jose Pulido <josecpulidoo@gmail.com>
2020-11-08 23:01:02 -0400

    fix index

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -23,68 +23,72 @@ import { SkillsGrid } from '../styles/SkillsGrid';
 
 function Hero() {
   return (
-    <TwoGridSection>
-      <ImageJose />
-      <FirstTitle>
-        <h1>I'm Jose Pulido.</h1>
-        <h1>Web/Shopify developer.</h1>
-        <p>
-          I build amazing tailor-made shopify stores. And i can make a really
-          great Guacamole 🥑.
-          <br /> If you have a design i can make it come to life.
-        </p>
-        <Link href="mailto:josecpulidoo@gmail.com">Let's work together</Link>
-      </FirstTitle>
-    </TwoGridSection>
+    <div>
+      <TwoGridSection>
+        <ImageJose />
+        <FirstTitle>
+          <h1>I'm Jose Pulido.</h1>
+          <h1>Web/Shopify developer.</h1>
+          <p>
+            I build amazing tailor-made shopify stores. And i can make a really
+            great Guacamole 🥑.
+            <br /> If you have a design i can make it come to life.
+          </p>
+          <Link href="mailto:josecpulidoo@gmail.com">Let's work together</Link>
+        </FirstTitle>
+      </TwoGridSection>
+    </div>
   );
 }
 function Experience() {
   return (
-    <ExperienceDiv>
-      <div>
-        <h3>Experience</h3>
-        <h1>05 years</h1>
-        <h2>Working as a Fullstack Developer</h2>
-        <p>
-          Currently working as a Shopify Developer in{' '}
-          <span>
-            <Link to="https://www.genium.io/">Genium.io</Link>
-          </span>
-        </p>
-      </div>
-      <div>
-        <div>My Skills</div>
-        <SkillsGrid>
-          <li>
-            <Shopify />
-          </li>
-          <li>
-            <Wordpress />
-          </li>
-          <li>
-            <Css />
-          </li>
-          <li>
-            <Javascript />
-          </li>
-          <li>
-            <Reactjs />
-          </li>
-          <li>
-            <Node />
-          </li>
-          <li>
-            <Gatsby />
-          </li>
-          <li>
-            <Graphql />
-          </li>
-        </SkillsGrid>
-        <div className="BtnProject">
-          <Link to="/projects">View my Projects</Link>
+    <div>
+      <ExperienceDiv>
+        <div>
+          <h3>Experience</h3>
+          <h1>05 years</h1>
+          <h2>Working as a Fullstack Developer</h2>
+          <p>
+            Currently working as a Shopify Developer in{' '}
+            <span>
+              <Link to="https://www.genium.io/">Genium.io</Link>
+            </span>
+          </p>
         </div>
-      </div>
-    </ExperienceDiv>
+        <div>
+          <div>My Skills</div>
+          <SkillsGrid>
+            <li>
+              <Shopify />
+            </li>
+            <li>
+              <Wordpress />
+            </li>
+            <li>
+              <Css />
+            </li>
+            <li>
+              <Javascript />
+            </li>
+            <li>
+              <Reactjs />
+            </li>
+            <li>
+              <Node />
+            </li>
+            <li>
+              <Gatsby />
+            </li>
+            <li>
+              <Graphql />
+            </li>
+          </SkillsGrid>
+          <div className="BtnProject">
+            <Link to="/projects">View my Projects</Link>
+          </div>
+        </div>
+      </ExperienceDiv>
+    </div>
   );
 }
 
