c031ae60e39ac5086f13cb2f7697643a26ee573d
Jose Pulido <josecpulidoo@gmail.com>
2020-11-08 22:46:33 -0400

    change in the index

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -21,6 +21,73 @@ import { FirstTitle } from '../styles/FistTitle';
 import { ExperienceDiv } from '../styles/ExperienceDiv';
 import { SkillsGrid } from '../styles/SkillsGrid';
 
+function Hero() {
+  return (
+    <TwoGridSection>
+      <ImageJose />
+      <FirstTitle>
+        <h1>I'm Jose Pulido.</h1>
+        <h1>Web/Shopify developer.</h1>
+        <p>
+          I build amazing tailor-made shopify stores. And i can make a really
+          great Guacamole 🥑.
+          <br /> If you have a design i can make it come to life.
+        </p>
+        <Link href="mailto:josecpulidoo@gmail.com">Let's work together</Link>
+      </FirstTitle>
+    </TwoGridSection>
+  );
+}
+function Experience() {
+  return (
+    <ExperienceDiv>
+      <div>
+        <h3>Experience</h3>
+        <h1>05 years</h1>
+        <h2>Working as a Fullstack Developer</h2>
+        <p>
+          Currently working as a Shopify Developer in{' '}
+          <span>
+            <Link to="https://www.genium.io/">Genium.io</Link>
+          </span>
+        </p>
+      </div>
+      <div>
+        <div>My Skills</div>
+        <SkillsGrid>
+          <li>
+            <Shopify />
+          </li>
+          <li>
+            <Wordpress />
+          </li>
+          <li>
+            <Css />
+          </li>
+          <li>
+            <Javascript />
+          </li>
+          <li>
+            <Reactjs />
+          </li>
+          <li>
+            <Node />
+          </li>
+          <li>
+            <Gatsby />
+          </li>
+          <li>
+            <Graphql />
+          </li>
+        </SkillsGrid>
+        <div className="BtnProject">
+          <Link to="/projects">View my Projects</Link>
+        </div>
+      </div>
+    </ExperienceDiv>
+  );
+}
+
 export default function HomePage() {
   return (
     <>
@@ -28,66 +95,8 @@ export default function HomePage() {
         <title>Welcome 🥑</title>
       </SEO>
       <div>
-        <TwoGridSection>
-          <ImageJose />
-          <FirstTitle>
-            <h1>I'm Jose Pulido.</h1>
-            <h1>Web/Shopify developer.</h1>
-            <p>
-              I build amazing tailor-made shopify stores. And i can make a
-              really great Guacamole 🥑.
-              <br /> If you have a design i can make it come to life.
-            </p>
-            <Link href="mailto:josecpulidoo@gmail.com">
-              Let's work together
-            </Link>
-          </FirstTitle>
-        </TwoGridSection>
-        <ExperienceDiv>
-          <div>
-            <h3>Experience</h3>
-            <h1>05 years</h1>
-            <h2>Working as a Fullstack Developer</h2>
-            <p>
-              Currently working as a Shopify Developer in{' '}
-              <span>
-                <Link to="https://www.genium.io/">Genium.io</Link>
-              </span>
-            </p>
-          </div>
-          <div>
-            <div>My Skills</div>
-            <SkillsGrid>
-              <li>
-                <Shopify />
-              </li>
-              <li>
-                <Wordpress />
-              </li>
-              <li>
-                <Css />
-              </li>
-              <li>
-                <Javascript />
-              </li>
-              <li>
-                <Reactjs />
-              </li>
-              <li>
-                <Node />
-              </li>
-              <li>
-                <Gatsby />
-              </li>
-              <li>
-                <Graphql />
-              </li>
-            </SkillsGrid>
-            <div className="BtnProject">
-              <Link to="/projects">View my Projects</Link>
-            </div>
-          </div>
-        </ExperienceDiv>
+        <Hero />
+        <Experience />
       </div>
     </>
   );
