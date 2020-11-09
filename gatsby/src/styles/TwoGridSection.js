034c8600f858261c501fd40f83960b87d3535b7d
Jose Pulido <josecpulidoo@gmail.com>
2020-11-08 23:38:15 -0400

    modify

--- gatsby/src/styles/TwoGridSection.js
+++ gatsby/src/styles/TwoGridSection.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const TwoGridSection = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(550px, 1fr));
   justify-content: center;
   align-items: center;
   height: 700px;
