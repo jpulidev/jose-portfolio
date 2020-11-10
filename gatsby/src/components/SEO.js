cbd9c6cbdc203afc8b2fd68306c3347ac904af18
Jose Pulido <josecpulidoo@gmail.com>
2020-11-10 17:37:44 -0400

    Seo Image Fix

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -28,7 +28,7 @@ export default function SEO({ children, location, description, title, image }) {
       <meta name="description" content={site.siteMetadata.description} />
       {/* Open graph */}
       {location && <meta property="og:url" content={location.href} />}
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={image || '/laptopcode.png'} />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta
         property="og:site_name"
